77a12663c27d69993feb840a5051078faf6adb15
pochtalon <pochtalon1010@gmail.com>
2023-10-29 21:00:04 +0200

    Created constants in JwtAuthenticationFilter;

--- src/main/java/mate/intro/repository/RoleRepository.java
+++ src/main/java/mate/intro/repository/RoleRepository.java
@@ -5,6 +5,5 @@ import mate.intro.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
     Optional<Role> findRoleByName(Role.RoleName roleName);
 }
