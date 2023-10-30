0d1aaf207809e293e28c0f74cc22b2bdc64032a3
pochtalon <pochtalon1010@gmail.com>
2023-10-30 09:37:05 +0200

    Created controllers; Added Swagger dependency;

---
+++ src/main/java/mate/intro/controller/UserController.java
@@ -0,0 +1,14 @@
+package mate.intro.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "User management", description = "Endpoints for managing users")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/users")
+public class UserController {
+
+}
