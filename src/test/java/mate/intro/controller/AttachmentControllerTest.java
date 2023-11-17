3620d6490fffe7442d89d5d46bbc15678fcf3aa7
pochtalon <pochtalon1010@gmail.com>
2023-11-17 16:05:42 +0200

    Finished AuthControllerTest;

---
+++ src/test/java/mate/intro/controller/AttachmentControllerTest.java
@@ -0,0 +1,8 @@
+package mate.intro.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AttachmentControllerTest {
+
+}
